99e8bfcb41e0c509b3c25683388e1dd0e0cf55c7
unknown <nguyentantaitcag2000@gmail.com>
2023-12-02 18:03:08 +0700

    update recomment

--- python/recommend.py
+++ python/recommend.py
@@ -3,10 +3,20 @@ import os
 
 import pandas as pd
 import difflib
+from pyvi import ViTokenizer
+from string import punctuation
 class Recommend:
     def __init__(self):
         self.rootFolder = os.path.dirname(os.path.abspath(__file__))
-    
+        # Load stopword
+        self.stop_word = []
+        with open(os.path.join(self.rootFolder, 'models/stop_word.txt'),encoding="utf-8") as f :
+            text = f.read()
+            for word in text.split() :
+                self.stop_word.append(word)
+            f.close()
+        punc = list(punctuation)
+        self.stop_word = self.stop_word + punc
         # Load mô hình
         with open(os.path.join(self.rootFolder, 'models/recommend/vectorizer_model.pkl'), 'rb') as f:
             self.vectorizer = pickle.load(f)
@@ -19,10 +29,13 @@ class Recommend:
 
         # Load DataFrame
         self.products_data = pd.read_pickle(os.path.join(self.rootFolder, 'models/recommend/products_data.pkl'))
-    def search(self, name_product):
-        list_of_all_titles = self.products_data['name_product'].tolist()
 
-        find_close_match = difflib.get_close_matches(name_product, list_of_all_titles)
+        self.list_of_all_titles = self.products_data['name_product'].tolist()
+    def search(self, name_product):
+        
+        name_product = self.pre_data_progress(name_product)
+
+        find_close_match = difflib.get_close_matches(name_product, self.list_of_all_titles)
 
         if find_close_match == []:
             return []
@@ -41,6 +54,20 @@ class Recommend:
                 title_from_index = self.products_data.at[index, 'name_product']
                 id_product = self.products_data.at[index, 'id_product'].item()  # Sử dụng item() để chuyển đổi numpy.int64
 
-                results.append({"ID_Product": id_product, "Name_Product": title_from_index})
+                results.append({"ID_Product": id_product, "Name_Product": title_from_index.replace('_',' ')})
 
             return results
+    def pre_data_progress(self, text):
+    # Đưa chuỗi về chữ thường và trim
+        text_lower = text.lower().strip()
+        # Tokenize
+        text_token = ViTokenizer.tokenize(text_lower)
+        # Loại bỏ stop_word và punctuation
+        sentence = ""
+
+        for word in text_token.split(" "):
+            if (word not in self.stop_word):
+
+                if ("_" in word) or (word.isalpha() == True) or word.isdigit():
+                    sentence += word + " "
+        return sentence.strip()
